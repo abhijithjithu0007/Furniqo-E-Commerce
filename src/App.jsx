1560a2dfc53a0cc42e7758c4c564283f06940cea
abhijith <abhijithabhi36151@gmail.com>
2024-06-24 16:17:51 +0530

    Navbar Created simply

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import Navbar from "./component/Navbar"
 export default function App() {
   return (
     <h1 className="text-3xl text-red-700 font-bold underline">
-      Hello world!
+      <Navbar />
     </h1>
   )
 }
