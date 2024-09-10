3fa6b8985f2ea52276f3f21b0617da5e1e836a4e
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-09-10 14:22:08 +0530

    api origin dynamic

--- src/component/CartContext.jsx
+++ src/component/CartContext.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { createContext, useState, useEffect } from 'react'
 
+
 export const cartContext = createContext()
 const CartContextProvider = ({ children }) => {
 
@@ -11,10 +12,11 @@ const CartContextProvider = ({ children }) => {
 
   const [total, setTotal] = useState([])
   const [myPro, setMyPro] = useState([])
+  const apiorigin = import.meta.env.VITE_API_URL
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const resp = await axios.get(`https://ecommerce-backend-r65b.onrender.com/api/user/viewcartproducts/${id}`, {
+        const resp = await axios.get(`${apiorigin}/api/user/viewcartproducts/${id}`, {
           withCredentials: true
         });
         const data = resp.data.products || [];
