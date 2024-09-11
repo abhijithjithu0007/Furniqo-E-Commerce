8977219f2bd1d4085055f4583f91fce7e0decedc
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-09-11 16:03:59 +0530

    ui chnaged

--- src/Pages/LoadSpinner.jsx
+++ src/Pages/LoadSpinner.jsx
@@ -7,7 +7,7 @@ const LoadSpinner = () => {
   if (!load) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50 transition-opacity duration-300">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30 z-50 transition-opacity duration-300">
       <RingLoader color="#FF6347" size={80} speedMultiplier={1.5} />
     </div>
   );
