7ab2374dc497492c26a52119613f6b7c400bfb58
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-09-16 11:48:51 +0530

    axios insatnce chnaged

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance= axios.create({
-    baseURL:"http://localhost:5000"
+    baseURL:'https://ecommerce-backend-r65b.onrender.com'
+    
    
 })
 
@@ -9,4 +10,4 @@ export default axiosInstance
 
 
 
-//     'https://ecommerce-backend-r65b.onrender.com'
+// "http://localhost:5000"
