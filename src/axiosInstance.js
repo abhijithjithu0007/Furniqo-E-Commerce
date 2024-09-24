f3a0d4148ae5d7504f64625a6f0870e57b89fa7b
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-09-24 14:41:18 +0530

    load spinner changed

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
 const axiosInstance= axios.create({
-    baseURL:    'https://ecommerce-backend-r65b.onrender.com'
-    
-   
-    
-   
+    baseURL:'https://ecommerce-backend-r65b.onrender.com'
 })
 
 export default axiosInstance
 
 
 
-//       "http://localhost:5000"
+//         'http://localhost:5000'
+   
+       
 
      
    
