2a0b5a0fd08d8074234d2b12a78b80541caf56aa
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-09-24 14:43:38 +0530

    just chnaged size of spinner

--- src/Components/LoadSpinner.jsx
+++ src/Components/LoadSpinner.jsx
@@ -8,7 +8,7 @@ const LoadSpinner = () => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30 z-50 transition-opacity duration-300">
-      <PropagateLoader color="#cd4632" size={80} speedMultiplier={1.5} />
+      <PropagateLoader color="#cd4632" size={15} speedMultiplier={1.5} />
     </div>
   );
 };
